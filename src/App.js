8bb2ed64ce3f316050194643a418896f73cb8388
leeos20k <leeos20k@naver.com>
2023-07-10 16:56:15 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  //검색어로 영화데이터 요청 async/await는 쌍으로 사용
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6bfc4a64`;
+    //자바스크립트는 비동기므로 await를 붙여서 영화데이터를 다받고 다음코드 실행
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      //결과가 있을경우!
+      setMovies(responseJson.Search);
+    }
+  };
+
+  //검색어가 바뀔시 한번 함수 실행
+  useEffect(() => {
+    if (searchValue.length > 3) {
+      getMovieRequest(searchValue);
+    }
+  }, [searchValue]);
+
+  //브라우저 저장소에서 선호작 영화들을 가져옴 앱 시작시 1번 실행
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem("favourites"));
+    if (movieFavourites) {
+      setFavourites(movieFavourites);
+    }
+  }, []);
+
+  //브라우저에 저장하기
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("favourites", JSON.stringify(items));
+  };
+
+  //선호작에 영화를 추가하기
+  const addFavouriteMovie = (movie) => {
+    const newList = [...favourites, movie]; //선호작 리스트에 + 영화하나 추가
+    setFavourites(newList); //새 선호작으로 업데이트
+    saveToLocalStorage(newList); // 저장하기
+  };
+
+  //선호작을 제거하는 함수(id가 같으면 필터로 제거)
+  const removeMovie = (movie) => {
+    const newList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newList);
+    saveToLocalStorage(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+      <div className="row align-items-center my-4">
+        <MovieListHeading heading="영화 검색과 선호작 등록" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <ScrollContainer className="row scroll-container">
+        <MovieList
+          movies={movies}
+          handleClick={addFavouriteMovie}
+          addMovie={true}
+        />
+      </ScrollContainer>
+      <div className="row align-items-center my-4">
+        <MovieListHeading heading="내 선호작" />
+      </div>
+      <ScrollContainer className="row scroll-container">
+        <MovieList
+          movies={favourites}
+          handleClick={removeMovie}
+          addMovie={false}
+        />
+      </ScrollContainer>
     </div>
   );
 }
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MovieList = (props) => {
+  return (
+    <>
+      {props.movies.map((movie) => (
+        <div className="image-container d-flex m-3" key={movie.imdbID}>
+          <img src={movie.Poster} alt="movie"></img>
+          <div
+            className="overlay d-flex align-items-center justify-content-center"
+            onClick={() => props.handleClick(movie)}
+          >
+            <span className="me-2">
+              {props.addMovie ? "선호작 추가" : "선호작 제거"}
+            </span>
+            <span>{props.addMovie ? "🧡" : "❌"}</span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
